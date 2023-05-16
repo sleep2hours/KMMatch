c565668b3889cd6faf2c9be837fecd2216539af4
sleep2hours <1569171127@qq.com>
2023-05-16 22:52:30 +0800

    Add KMMatch.

--- include/KMMatch.h
+++ include/KMMatch.h
@@ -1,49 +1,52 @@
 #ifndef GROUP_TRANSFORM_H_
 #define GROUP_TRANSFORM_H_
-#include <ros/ros.h>
-#include <vector>
-#include <nav_msgs/Odometry.h>
-#include <sensor_msgs/Imu.h>
-#include <iostream>
-#include <string>
+#include "hungarian_optimizer.h"
+#include "quadrotor_msgs/PositionCommand.h"
 #include <Eigen/Eigen>
 #include <geometry_msgs/PoseStamped.h>
-#include "hungarian_optimizer.h"
+#include <iostream>
 #include <message_filters/subscriber.h>
-#include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
-#include "quadrotor_msgs/PositionCommand.h"
+#include <message_filters/time_synchronizer.h>
+#include <nav_msgs/Odometry.h>
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <string>
+#include <vector>
 using namespace message_filters;
-typedef sync_policies::ApproximateTime<quadrotor_msgs::PositionCommand,quadrotor_msgs::PositionCommand,quadrotor_msgs::PositionCommand,quadrotor_msgs::PositionCommand,quadrotor_msgs::PositionCommand> MySyncPolicy;
-namespace KMMatch{
-    class KMMatch{
-        private:
-        int drone_num;
-        bool target_receive,first_init;
-        std::vector<Eigen::Vector3d> present_pos;
-        std::vector<Eigen::Vector3d> target_pos;
-        std::vector<message_filters::Subscriber<quadrotor_msgs::PositionCommand>*> pos_sub;
+typedef sync_policies::ApproximateTime<
+    quadrotor_msgs::PositionCommand, quadrotor_msgs::PositionCommand,
+    quadrotor_msgs::PositionCommand, quadrotor_msgs::PositionCommand,
+    quadrotor_msgs::PositionCommand>
+    MySyncPolicy;
+namespace KMMatch {
+class KMMatch {
+private:
+  int drone_num;
+  bool target_receive, first_init;
+  std::vector<Eigen::Vector3d> present_pos;
+  std::vector<Eigen::Vector3d> target_pos;
+  std::vector<message_filters::Subscriber<quadrotor_msgs::PositionCommand> *>
+      pos_sub;
 
-        HungarianOptimizer<double> optimizer_;
-       
-        std::vector<ros::Publisher>goal_puber;
-        ros::Subscriber target_suber;
-        Synchronizer<MySyncPolicy> * sync;
+  HungarianOptimizer<double> optimizer_;
 
-        void OdomCallback(const quadrotor_msgs::PositionCommandConstPtr &msg1,const quadrotor_msgs::PositionCommandConstPtr &msg2,const quadrotor_msgs::PositionCommandConstPtr &msg3,const quadrotor_msgs::PositionCommandConstPtr &msg4,const quadrotor_msgs::PositionCommandConstPtr &msg5);
-        void TargetCallback(const geometry_msgs::PoseStampedPtr &msg);
+  std::vector<ros::Publisher> goal_puber;
+  ros::Subscriber target_suber;
+  Synchronizer<MySyncPolicy> *sync;
 
-        public:
-        KMMatch(){
+  void OdomCallback(const quadrotor_msgs::PositionCommandConstPtr &msg1,
+                    const quadrotor_msgs::PositionCommandConstPtr &msg2,
+                    const quadrotor_msgs::PositionCommandConstPtr &msg3,
+                    const quadrotor_msgs::PositionCommandConstPtr &msg4,
+                    const quadrotor_msgs::PositionCommandConstPtr &msg5);
+  void TargetCallback(const geometry_msgs::PoseStampedPtr &msg);
 
-        }
-        ~KMMatch(){
-
-        }
-        void init(ros::NodeHandle &nh);
-        void match();
-    };
-}
+public:
+  KMMatch() {}
+  ~KMMatch() {}
+  void init(ros::NodeHandle &nh);
+  void match();
+};
+} // namespace KMMatch
 #endif
-
-
